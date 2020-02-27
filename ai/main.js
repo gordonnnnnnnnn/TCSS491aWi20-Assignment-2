ed3a6fd8c3646bc4a93c2e69b2af41956fa9e048
gordonnnnnnnnn <gord@uw.edu>
2020-02-27 00:12:38 -0800

    handle database connection failure

--- ai/main.js
+++ ai/main.js
@@ -6,7 +6,12 @@ ASSET_MANAGER.queueDownload("./img/noise.png");
 ASSET_MANAGER.queueDownload("./img/paddle.png");
 
 ASSET_MANAGER.downloadAll(function () {
-    var socket = io.connect("http://24.16.255.56:8888");
+    var socket;
+    try {
+        socket = io.connect("http://24.16.255.56:8888");
+    } catch (error) {
+        console.log("Problems connecting to database.\nRemoving save and load buttons.");
+    }
 
     let canvas = document.getElementById('gameWorld');
     let ctx = canvas.getContext('2d');
@@ -20,15 +25,19 @@ ASSET_MANAGER.downloadAll(function () {
  
     gameEngine.init(ctx);
     gameEngine.start();
-
-    socket.on("load", function (data) {
-        gameEngine.setGameState(data);
-    });
-    document.getElementById("save").onclick = function () {
-        socket.emit("save", gameEngine.getGameState());
-    };
-    document.getElementById("load").onclick = function () {
-        socket.emit("load", {studentname: "Gordon McCreary", statename: "aiMode"});
-    };
+    if (socket) {
+        socket.on("load", function (data) {
+            gameEngine.setGameState(data);
+        });
+        document.getElementById("save").onclick = function () {
+            socket.emit("save", gameEngine.getGameState());
+        };
+        document.getElementById("load").onclick = function () {
+            socket.emit("load", {studentname: "Gordon McCreary", statename: "aiMode"});
+        };
+    } else {
+        document.getElementById('save').remove();
+        document.getElementById('load').remove();
+    }
     
 });
